c4c66b92ef50a58eb55bd6c5fd2abec91e9ec3e4
Gopal-Sable <sablegopal123@gmail.com>
2023-03-28 15:09:47 +0530

    Trusted Section created & contact page created

--- src/Contact.js
+++ src/Contact.js
@@ -35,7 +35,33 @@ const Contact = () => {
 
   return (
     <Wrapper>
-      <h1>Contact page</h1>
+      <h2 className="common-heading">Contact page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.3255601437277!2d75.32176601486397!3d19.868470286638182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdb9861a6d01f43%3A0x3a1b932d329091e9!2sGovernment%20College%20Of%20Engineering%2C%20Aurangabad!5e0!3m2!1sen!2sin!4v1679994384548!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowfullScreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/mrgvyzbz" method="post" className="contact-inputs">
+            <input
+              type="text"
+              name="username"
+              required
+              placeholder="Enter your username"
+              autoComplete="off"
+            />
+            <input type="email" name="Email" placeholder="Enter your Mail-ID" autoComplete="off" required />
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Enter your message ...." autoComplete="off" required></textarea>
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
     </Wrapper>
   );
 };
--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 import HeroSection from "./components/HeroSection";
+import Trusted from "./components/Trusted";
+import Services from "./components/Services";
 
 const Home = () => {
-  return <HeroSection name="Gops Store"/>
+  return <>
+    <HeroSection name="Gops Store"/>
+    <Services/>
+    <Trusted/>
+     </>
 };
 
 const Wrapper = styled.section`
