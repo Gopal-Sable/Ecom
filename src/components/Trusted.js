730b98531c64bdc5cfe5f1cbbae63c500dca80fc
Gopal-Sable <sablegopal123@gmail.com>
2023-03-29 12:38:49 +0530

    Footer and Error page created

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -7,35 +7,41 @@ const Trusted = () => {
 
                 <div className="slide">
                     <img
-                        src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+                        src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png"
                         alt="trusted-brands"
                     />
                 </div>
 
                 <div className="slide">
                     <img
-                        src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+                        src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png"
                         alt="trusted-brands"
                     />
                 </div>
 
                 <div className="slide">
                     <img
-                        src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+                        src="https://www.freepnglogos.com/uploads/logo-tokopedia-png/tokopedia-apa-itu-startup-pengertian-cara-memulai-dan-1.png"
                         alt="trusted-brands"
                     />
                 </div>
 
                 <div className="slide">
                     <img
-                        src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+                        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
                         alt="trusted-brands"
                     />
                 </div>
 
                 <div className="slide">
                     <img
-                        src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+                        src="https://www.freepnglogos.com/uploads/starbucks-logo-png-transparent-0.png"
+                        alt="trusted-brands"
+                    />
+                </div>
+                <div className="slide">
+                    <img
+                        src="https://www.freepnglogos.com/uploads/snapchat-logo-png-0.png"
                         alt="trusted-brands"
                     />
                 </div>
@@ -58,9 +64,18 @@ const Wrapper = styled.section`
     font-weight: bold;
   }
   img {
+      filter: gray; /* IE5+ */
+      -webkit-filter: grayscale(1); 
+      -webkit-transition: all .8s ease-in-out;
     min-width: 10rem;
+    max-width:15rem;
     height: 10rem;
   }
+  img:hover {
+    filter: none;
+      -webkit-filter: grayscale(0);
+      -webkit-transform: scale(1.01);
+    }
   .brand-section-slider {
     margin-top: 3.2rem;
     display: flex;
