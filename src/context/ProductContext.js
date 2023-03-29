257e44824336949cbd74470c830ac6b7013711f1
Gopal-Sable <sablegopal123@gmail.com>
2023-03-29 15:17:20 +0530

    created Reducer(action,payload)

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -2,28 +2,49 @@
 // provider {dilvery boy} ---transfer
 //consumer=> useContext Hook ---usecase
 
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
+import reducer from "../Reducer/productReducer";
 
 const AppContext = createContext();
 
-const API ="https://api.pujakaitem.com/api/products";
+const API = "https://api.pujakaitem.com/api/products";
+
+const initialState = {
+    isLoading: false,
+    isError: false,
+    featureProduts: [],
+    products: [],
+}
 
 const AppProvider = ({ children }) => {
-    const getProducts=async(url)=>{
-        const res=await axios.get(url);
-        const products=await res.data;
-        console.log(res);
+
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    
+    const getProducts = async (url) => {
+        dispatch({ type: "SET_LOADING" });
+
+        try {
+            const res = await axios.get(url);
+            const products = await res.data;
+            dispatch({ type: "SET_API_DATA", payload: products })
+        }
+        catch (error) {
+            dispatch({ type: "API_ERROR" });
+        }
     }
     useEffect(() => {
         getProducts(API);
     }, [])
-    return <AppContext.Provider value={{ nameUsed: "Gopal sable" }}>
+
+
+    return <AppContext.Provider value={{ ...state }}>
         {children}
     </AppContext.Provider>
 };
+
 //global context or custome context
 const useProductCotext = () => { return useContext(AppContext) };
-
 
 export { AppProvider, AppContext, useProductCotext };
