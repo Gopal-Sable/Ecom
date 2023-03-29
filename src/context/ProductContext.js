26bd7324f229d4c6e317e2630c553dbc50830c72
Gopal-Sable <sablegopal123@gmail.com>
2023-03-29 14:08:30 +0530

    API called

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -2,17 +2,28 @@
 // provider {dilvery boy} ---transfer
 //consumer=> useContext Hook ---usecase
 
-import { createContext,useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
-const AppContext=createContext();
+const AppContext = createContext();
 
-const AppProvider=({children})=>{
-    return <AppContext.Provider value={{nameUsed:"Gopal sable"}}>
+const API ="https://api.pujakaitem.com/api/products";
+
+const AppProvider = ({ children }) => {
+    const getProducts=async(url)=>{
+        const res=await axios.get(url);
+        const products=await res.data;
+        console.log(res);
+    }
+    useEffect(() => {
+        getProducts(API);
+    }, [])
+    return <AppContext.Provider value={{ nameUsed: "Gopal sable" }}>
         {children}
     </AppContext.Provider>
 };
 //global context or custome context
-const useProductCotext=()=>{return useContext(AppContext)};
+const useProductCotext = () => { return useContext(AppContext) };
 
 
-export {AppProvider,AppContext,useProductCotext};
+export { AppProvider, AppContext, useProductCotext };
